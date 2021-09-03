b985cde45dc5fb63a5c14c077947673e07ca2683
Hyeokgi <chinup1004@naver.com>
2021-09-03 17:07:07 +0900

    ReadFile

--- main.js
+++ main.js
@@ -5,15 +5,43 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+    var title = queryData.id;
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(queryData.id);
+
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <div id="grid">
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <div id="article">
+            <h2>${title}</h2>
+            <p>${description}
+            </p>
+          </div>
+        </div>
+      </body>
+      </html>
+      `;
+      response.end(template);
+    });
  
 });
 app.listen(3000);
