9ead8b7d59f4cd3154fefa36d5d7c07a3937edb5
Hyeokgi <chinup1004@naver.com>
2021-09-03 18:03:55 +0900

    Not Found

--- main.js
+++ main.js
@@ -5,43 +5,42 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
 
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <div id="grid">
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <div id="article">
-            <h2>${title}</h2>
-            <p>${description}
-            </p>
+    if(pathname === '/') {
+      fs.readFile(`data/${title}`, 'utf8', function(err, description){
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+          <link rel="icon" href="data:,">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <div id="grid">
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <div id="article">
+              <h2>${title}</h2>
+              <p>${description}
+              </p>
+            </div>
           </div>
-        </div>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    });
- 
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      response.writeHead(404);
+      response.end('<h1>Not found</h1>');
+    }
 });
 app.listen(3000);
