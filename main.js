fbe7de486df1f6e3103fc2cb5b17a9fb009d4623
Hyeokgi <chinup1004@naver.com>
2021-09-22 01:37:46 +0900

    Fix String Query err

--- main.js
+++ main.js
@@ -7,38 +7,46 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-
     if(pathname === '/') {
       fs.readFile(`data/${title}`, 'utf8', function(err, description){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-          <link rel="icon" href="data:,">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <div id="grid">
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <div id="article">
-              <h2>${title}</h2>
-              <p>${description}
-              </p>
+        if(title === undefined & Object.keys(queryData).length == 0){
+          title = 'Welcome';
+          description = 'Hello, Node.js';
+        }
+        if(description !== undefined){
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            <link rel="icon" href="data:,">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <div id="grid">
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <div id="article">
+                <h2>${title}</h2>
+                <p>${description}
+                </p>
+              </div>
             </div>
-          </div>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
-    } else {
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+          } else {
+            response.writeHead(404);
+            response.end('<h1>Not found</h1>');
+          }
+        });
+      } else {
       response.writeHead(404);
       response.end('<h1>Not found</h1>');
     }
